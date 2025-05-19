7b3a5f2cdee8522eab55c02d5d4fa13c333d386e
KeaneHh <hk1372699026@gmail.com>
2025-05-19 17:00:20 +0800

    change paused logic

--- types/blockinfo.go
+++ types/blockinfo.go
@@ -30,9 +30,12 @@ type StateRoot struct {
 }
 
 type SignRequest struct {
-	StateRoot   string `json:"state_root"`
-	Signature   []byte `json:"signature"`
-	SignAddress string `json:"sign_address"`
+	StateRoot     string `json:"state_root"`
+	Signature     []byte `json:"signature"`
+	L2BlockNumber uint64 `json:"l2_block_number"`
+	L1BlockHash   string `json:"l1_block_hash"`
+	L1BlockNumber uint64 `json:"l1_block_number"`
+	SignAddress   string `json:"sign_address"`
 }
 
 type OperatorPaused struct {
